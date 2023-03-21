a5e1a82d9e748fe4d65b8011fb264baca777e36b
yangle <aylyang@outlook.com>
2023-03-21 16:32:09 +0800

    20230321 完成路由配置，利用filter实现请求和响应的加解密问题

---
+++ MicroService-Gateway/src/main/java/com/micro/service/bean/BodyHandlerFunction.java
@@ -0,0 +1,20 @@
+package com.micro.service.bean;
+
+import org.reactivestreams.Publisher;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
+
+/**
+ * @Classname BodyHandlerFunction
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2023/3/16 15:53
+ * @Created by yangle
+ */
+public interface BodyHandlerFunction extends
+        BiFunction<ServerHttpResponse, Publisher<? extends DataBuffer>, Mono<Void>> {
+}
+
